d8c95a9070ddf7e3c20f9d431fb8c67e479dd2b7
Dean Wampler <dean.wampler@ibm.com>
2026-04-06 14:59:01 -0400

    Updated features
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

--- check-external-links.sh
+++ check-external-links.sh
@@ -13,10 +13,20 @@ For markdown files, it prints anchor tags ("[name](url)") that don't
 have a "{:target=...}" appended to them. For HTML files, it looks for 
 the equivalent "<a href="..." target="...">...</a>".
 
+It also handles two '{{site.*url}}' definitions we use for referencing 
+commonly used external sites:
+* '{{site.glossaryurl}}' for the separate glossary site.
+* '{{site.gh_edit_repository}}' for the GitHub repo URL.
+
+It attempts to correctly ignore image URLs, e.g., '![label](https://example.com/image.png)',
+by looking at the file extension (jpg|jpeg|png|svg|mp3|mp4). This means
+that if a URL that should have a target happens to have one of those strings,
+it won't be checked for the target!
+
 It doesn't exit with an error if such links are found, because in some
 cases, this might be intentional.
 
-Usage: $script [-h|--help] [-n|--noop] [path1 ...]
+Usage: $script [-h|--help] [-n|--noop] [-v|--verbose] [path1 ...]
 
 Where the arguments are the following:
 -h | --help            Print this message and exit
@@ -27,6 +37,8 @@ Where the arguments are the following:
 path1 ...              Check these paths. Directories will be visited recursively.
                        Default: All markdown and HTML files under "$default_path",
                        excluding files under "_site" and "_sass".
+NOTES:
+1. Skips files found under "temp", "tmp", "_site", and "_sass" directories.
 EOF
 }
 
@@ -78,14 +90,21 @@ do
 		dir=$([[ -d "$path" ]] && echo "(directory)")
 		echo "$path $dir"
 	fi
-	$eg -nHoR '\(https?[^)]+\)(\S*)' \
+	$NOOP $eg -nHoR '\(https?[^)]+\)(\S*)' \
 		--include '*.markdown' --include '*.md' \
+ 		--exclude-dir 'temp' --exclude-dir 'tmp' \
 		--exclude-dir '_site' --exclude-dir '_sass' \
-		$path | $eg -v 'target='
-	$eg -nHoR '\(\{\{site.glossaryurl\}\}[^)]*\)(\S*)' \
+		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
+	$NOOP $eg -nHoR '\(\{\{site.glossaryurl\}\}[^)]*\)(\S*)' \
 		--include '*.markdown' --include '*.md' \
+ 		--exclude-dir 'temp' --exclude-dir 'tmp' \
 		--exclude-dir '_site' --exclude-dir '_sass' \
-		$path | $eg -v 'target='
+		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
+	$NOOP $eg -nHoR '\(\{\{site.gh_edit_repository\}\}[^)]*\)(\S*)' \
+		--include '*.markdown' --include '*.md' \
+ 		--exclude-dir 'temp' --exclude-dir 'tmp' \
+		--exclude-dir '_site' --exclude-dir '_sass' \
+		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
 done
 
 [[ -n "$VERBOSE" ]] && echo "Checking HTML files:"
@@ -96,8 +115,9 @@ do
 		dir=$([[ -d "$path" ]] && echo "(directory)")
 		echo "$path $dir"
 	fi
-	$eg -nHoR '<a\s*href="https?[^>]+>' \
+	$NOOP $eg -nHoR '<a\s*href="https?[^>]+>' \
 		--include '*.html' \
+ 		--exclude-dir 'temp' --exclude-dir 'tmp' \
 		--exclude-dir '_site' --exclude-dir '_sass' \
-		$path | $eg -v 'target='
+		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
 done
